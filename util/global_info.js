a54f248dcdee056be308583533180e6a057ff2f0
luciano <luciano.cardoso@codeitsolutions.com.br>
2018-07-23 14:28:13 -0300

    global removed.

--- util/global_info.js
+++
@@ -1,6 +0,0 @@
-let Global = {
-    hash: '',// A TIME STAMP + YOUR PRIVATE KEY  + PUBLIC KEY
-    apikey:'',// YOUR PUBLIC KEY
-    charactersUrl: "http://gateway.marvel.com/v1/public/characters",
-    ts: "1"
-}; 
